eff41715c7220179ca979121b97793ebe1d3a8b3
malyshev.v <malyshev.v@BP-IT-ADMIN-01>
2025-01-30 12:20:02 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Взаимодействие_с_AD
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
